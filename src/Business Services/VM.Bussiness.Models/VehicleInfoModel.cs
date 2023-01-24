b0fda6344a23ba3d3a52ea38ef1e06ed57718185
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-24 21:49:31 +0500

    Initial Commit

---
+++ src/Business Services/VM.Bussiness.Models/VehicleInfoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VM.Bussiness.Models
+{
+    public class VehicleInfoModel
+    {
+        public int VId { get; set; }
+        public string? Vehicle { get; set; } = string.Empty;
+        public String? VehicleModel { get; set; } = string.Empty;
+        public String? VehicleNumber { get; set; } = string.Empty;
+        public string? OwnerName { get; set; } = string.Empty;
+        public string? ContactNo { get; set; } = string.Empty;
+        public string? EmailAddress { get; set; } = string.Empty;
+    }
+}
---
+++ src/Repositories/VM.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace VM.Data
+{
+    public class Class1
+    {
+
+    }
+}
